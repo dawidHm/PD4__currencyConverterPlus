3e2d3ab06079d19f3f65fb783066aa0854c8c92e
Dawid Humienik <dawid.humienik@gmail.com>
2022-01-06 21:15:13 +0100

    script updated

--- js/script.js
+++ js/script.js
@@ -1,63 +1,79 @@
-    let formElement = document.querySelector(".js-form");
-    let currencySelectorElement = document.querySelector(".js-currencySelector");
-    let currencySymbolElement = document.querySelector(".js-currencySymbol");
-    let currentRateElement = document.querySelector(".js-currentRate");
-    let amountSymbolElement = document.querySelector(".js-amountSymbol");
-    let amountElement = document.querySelector(".js-amount");
-    let convertResultElement = document.querySelector(".js-convertResult");
+{
+    const formElement = document.querySelector(".js-form");
+    const amountElement = document.querySelector(".js-amount");
+    const currencyOptionElement = document.querySelector(".js-currencyOption");
+    const currentRateElement = document.querySelector(".js-currentRate");
+    const convertResultElement = document.querySelector(".js-convertResult");
 
-    let rateUSD = 4.04;
-    let rateEUR = 4.57;
-
-    formElement.addEventListener("input", () => { 
-        switch (currencySelectorElement.value) {
+    const calculateResult = (amount, currency) => {
+        switch (currency) {
             case "eur-pln":
-                currentRateElement.value = rateEUR;
-                currencySymbolElement.innerText = "€";
-                amountSymbolElement.innerText = "€";
+                result = amount * currentRateElement.value;
+                convertResultElement.innerHTML = `${amount.toFixed(2)} EUR = <strong>${result.toFixed(2)} PLN </strong>`;
                 break;
             case "usd-pln":
-                currentRateElement.value = rateUSD;
-                currencySymbolElement.innerText = "$";
-                amountSymbolElement.innerText = "$";
+                result = amount * currentRateElement.value;
+                convertResultElement.innerHTML = `${amount.toFixed(2)} USD = <strong>${result.toFixed(2)} PLN </strong>`;
                 break;
             case "pln-eur":
-                currentRateElement.value = rateEUR;
-                currencySymbolElement.innerText = "€";
-                amountSymbolElement.innerText = "zł";
+                result = amount / currentRateElement.value;
+                convertResultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} EUR </strong>`;
                 break;
             case "pln-usd":
-                currentRateElement.value = rateUSD;
-                currencySymbolElement.innerText = "$";
-                amountSymbolElement.innerText = "zł";
+                result = amount / currentRateElement.value;
+                convertResultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} USD </strong>`;
                 break;
-        }
+            case "customOption":
+                result = amount / currentRateElement.value;
+                convertResultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} X </strong>`;
+                break;
+        }     
+    }
 
-    });
+    const currency = currencyOptionElement.value;
 
     formElement.addEventListener("submit", (event) => {
         event.preventDefault();
-        
-        let currency = currentRateElement.value;
-        let amount = amountElement.value;
-        let convert;
 
-        switch (currencySelectorElement.value) {
-            case "eur-pln":
-                convert = (currency * amount);
-                convertResultElement.innerText = amount + " EUR = " + convert.toFixed(2) + " PLN" ;
-                break;
-            case "usd-pln":
-                convert = (currency * amount);
-                convertResultElement.innerText = amount + " USD = " + convert.toFixed(2) + " PLN" ;
-                break;
-            case "pln-eur":
-                convert = (amount / currency);
-                convertResultElement.innerText = amount + " PLN = " + convert.toFixed(2) + " EUR" ;
-                break;
-            case "pln-usd":
-                convert = (amount / currency);
-                convertResultElement.innerText = amount + " PLN = " + convert.toFixed(2) + " USD" ;
-                break;
-        }     
+        const amount = +amountElement.value;
+
+        const result = calculateResult(amount, currencyOptionElement.value);
     });
+
+    const currencySymbolElement = document.querySelector(".js-currencySymbol");
+    const amountSymbolElement = document.querySelector(".js-amountSymbol");
+
+    const changeLabels = () => {
+        formElement.addEventListener("input", () => { 
+            switch (currencyOptionElement.value) {
+                case "eur-pln":
+                    currentRateElement.value = 4.57;
+                    currencySymbolElement.innerText = "€";
+                    amountSymbolElement.innerText = "€";
+                    break;
+                case "usd-pln":
+                    currentRateElement.value = 4.04;
+                    currencySymbolElement.innerText = "$";
+                    amountSymbolElement.innerText = "$";
+                    break;
+                case "pln-eur":
+                    currentRateElement.value = 4.57;
+                    currencySymbolElement.innerText = "€";
+                    amountSymbolElement.innerText = "zł";
+                    break;
+                case "pln-usd":
+                    currentRateElement.value = 4.04;
+                    currencySymbolElement.innerText = "$";
+                    amountSymbolElement.innerText = "zł";
+                    break;
+                case "customOption":
+                    currencySymbolElement.innerText = "(X)";
+                    amountSymbolElement.innerText = "zł";
+                    break;
+            }
+        });
+    
+    }
+    
+    changeLabels();
+}
